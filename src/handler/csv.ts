2390dc229f5cf6c041f13ff7ad4119184f6e7076
jskadomoto <jonesseiji9@gmail.com>
2024-08-03 15:09:15 -0300

    feat(csv): implement pagination and uuid

--- src/handler/csv.ts
+++ src/handler/csv.ts
@@ -5,37 +5,54 @@ import csvParser from 'csv-parser'
 import { cpf } from 'cpf-cnpj-validator'
 import { formatCurrencyToBRL } from '../utils/currency'
 import { isValidInstallments } from '../utils/installments'
+import { v4 as uuidv4 } from 'uuid'
 
-export const parseCsv = async (): Promise<ICsvData[]> => {
+export const parseCsv = async (
+	page: number,
+	limit: number
+): Promise<ICsvData[]> => {
 	const results: ICsvData[] = []
-	const csvPath = path.join(__dirname, '..', 'mock', 'data.csv') /* cria um path para o arquivo CSV, localizado em: /src/mock/data.csv */
+	const csvPath = path.join(
+		__dirname,
+		'..',
+		'mock',
+		'data.csv'
+	) /* cria um path para o arquivo CSV, localizado em: /src/mock/data.csv */
+
+	let count = 0
+	const startCount = (page - 1) * limit
+	const endCount = startCount + limit
 
 	return new Promise((resolve, reject) => {
 		fs.createReadStream(csvPath)
 			.pipe(csvParser())
-			.on('data', (data: ICsvData) =>
-				results.push({
-					nrCpfCnpj: data.nrCpfCnpj,
-					vlTotal: data.vlTotal,
-					qtPrestacoes: data.qtPrestacoes,
-					vlPresta: data.vlPresta,
-					vlMora: data.vlMora ?? null,
-				})
-			)
+			.on('data', (data: ICsvData) => {
+				if (count > startCount && count < endCount)
+					results.push({
+						nrCpfCnpj: data.nrCpfCnpj,
+						vlTotal: data.vlTotal,
+						qtPrestacoes: data.qtPrestacoes,
+						vlPresta: data.vlPresta,
+						vlMora: data.vlMora ?? null,
+					})
+				count++
+			})
 			.on('end', () => resolve(results))
 			.on('error', (error) => reject(error))
 	})
 }
 
-export const processCsvData = async (): Promise<
-	IValidatedCsvDataResponse[]
-> => {
-  const data = await parseCsv()
-  
-  if(!data) return Promise.reject('Erro ao processar o CSV')
+export const processCsvData = async (
+	page: number,
+	limit: number
+): Promise<IValidatedCsvDataResponse[]> => {
+	const data = await parseCsv(page, limit)
 
-	const output = data?.map((it) => {
+	if (!data) return Promise.reject('Erro ao processar o CSV')
+
+	const output = data?.map((it, index) => {
 		return {
+			id: uuidv4(),
 			...it,
 			isValidCpfOrCnpj: cpf.isValid(it.nrCpfCnpj),
 			formattedVlTotal: formatCurrencyToBRL(it.vlTotal),
